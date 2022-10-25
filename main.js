32f03644d312f0b62e0d186681c49d80b26e3d1d
Ifycode <marychord@gmail.com>
2022-10-25 20:49:26 +0100

    cycle-through-images

--- main.js
+++ main.js
@@ -7,33 +7,40 @@ $("span").animate({left: '250px',
 opacity: '0.5',
 height: '150px',
 width: '150px'},5000);
-contentsParent.prepend('<img alt="webpage" src="https://m.media-amazon.com/images/I/81-AYXbCq4L._AC_UX500_.jpg"/>');
 
 let count = 1;
-let images = ['image-1.jpg', 'image-2.jpg', 'image-3.jpg'];
+let images = ['https://m.media-amazon.com/images/I/81-AYXbCq4L._AC_UX500_.jpg', 'image-1.jpg', 'image-2.jpg', 'image-3.jpg'];
 
-// function zeroCount() {
-//   if (count === 0) {
-//     console.log(count);
-//   }
-// }
+contentsParent.prepend(`
+  <section>
+    <div>
+      <img id="image-cycle" class="img" alt="webpage" src="${images[0]}"/>
+    </div>
+    <button id="myBtn">Cycle through image</button>
+  </section>
+`);
 
-//zeroCount();
-
+// Using intervals to cycle through
 $(document).ready(function() {
-    $('#myBtn').on('click', function() {
-      // zeroCount();
-      if (count === images.length) {
-        count = 0;
-        $('div').css('background-image', `url(https://m.media-amazon.com/images/I/81-AYXbCq4L._AC_UX500_.jpg)`);
-      }
-      $('div').css('background-image', `url(images/${images[count]})`);
-      console.log(images[count]);
-      count += 1;
-      console.log(count);
-      // if (count > 0) {
-       
-      // }
-      // $('div').addClass('pm-gallery-post-item-container').css('background-image', 'url(img/gallery/2nd-white-coat' + i + '.jpg)');
-    });
-  });
+  function cycleThroughImages() {
+    $('#image-cycle').attr("src", `/images/${images[count]}`);
+    if (count >= images.length) {
+      count = 0;
+      $('#image-cycle').attr("src", `${images[count]}`);
+    }
+    count += 1;
+  }
+  setInterval(cycleThroughImages,1500);
+});
+
+// Using button to cycle through
+// $(document).ready(function() {
+//   $('#myBtn').on('click', function() {
+//     $('#image-cycle').attr("src", `/images/${images[count]}`);
+//     if (count >= images.length) {
+//       count = 0;
+//       $('#image-cycle').attr("src", `${images[count]}`);
+//     }
+//     count += 1;
+//   });
+// });
